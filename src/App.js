cd6c8b0fbd73cbf8709114792a79fcab30d1429c
nestorLanex <nestor@lanexus.com>
2019-05-30 10:59:34 +0800

    initial ToDoList project release

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {Component, useState} from 'react';
 import logo from './logo.svg';
+import TodoTable from './TodoTable.js';
+import Form from './ToDoForm.js';
 import './App.css';
+import {StatusContext} from './ContextList';
 
 function App() {
+  const [taskList, setTask] = useState([]);
+  const contentstate = {
+    states: {taskList},
+    stateSetters: {createTask,resetList}
+  }
+
+  function createTask(task){
+    var rows = [...taskList];
+    rows.push(task);
+    setTask(rows);
+  }
+
+  function resetList(){
+    console.log('resetting list')
+    setTask([]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <StatusContext.Provider value={contentstate}>
+        <div style={{width:"60%", marginLeft:"20%"}} align="Center">
+          <p align="Center"> Hello World </p>
+          <Form />
+          <TodoTable/>
+        </div>
+      </StatusContext.Provider>
     </div>
   );
 }
+
 
 export default App;
